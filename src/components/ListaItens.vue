26c5fb13a537288f1d660f0ed9fb7129813c8a93
Leonardo Braga <leonardobraga24@gmail.com>
2023-12-18 13:31:06 -0300

    Add list and stating v-for

--- src/components/ListaItens.vue
+++ src/components/ListaItens.vue
@@ -1,14 +1,79 @@
 <template>
     <div>
       <h1>{{ title }}</h1>
+      <!-- <template> -->
+      <v-card v-for="item in items" :key="item.id"
+        class="mx-auto"
+        max-width="344"
+        outlined
+      >
+        <v-list-item three-line>
+          <v-list-item-content>
+            <div class="text-overline mb-4">
+              Item name 
+            </div>
+            <v-list-item-title class="text-h5 mb-1">
+              Coca-Cola {{ item.nameItem }}
+            </v-list-item-title>
+            <v-list-item-subtitle>2L {{ item.desc }}</v-list-item-subtitle>
+          </v-list-item-content>
+
+          <v-list-item-avatar
+            tile
+            size="80"
+            color="grey"
+          ></v-list-item-avatar>
+        </v-list-item>
+
+        <v-card-actions>
+          <v-btn
+            outlined
+            rounded
+            text
+          >
+            Edit
+          </v-btn>
+          <v-btn
+            outlined
+            rounded
+            text
+          >
+            Delete
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    <!-- </template> -->
     </div>
 </template>
   
   <script>
+  
   export default {
-    name: 'ListaItens',
+    data: function() {
+      return {
+        // items: [{
+        //   id: String,
+        //   nameItem: String,
+        //   desc: String
+        // }],
+      }
+    },
+    name: 'ListaItens', 
     props: {
-        title: String
+        title: String,
+    },
+    created() {
+      // var self = this;
+      //   axios.get('/meals')
+      //       .then(function (response) {
+      //       self.meal_choices = response.data;
+      //       console.log(response.data);
+      //     })
+      //     .catch(function (error) {
+      //       console.log(error);
+      //     });
+      const items = [{ id: '1', nameItem: "coca", desc: "2l" }, { id: '2', nameItem: "coca2", desc: "3l" }]
+      console.log(items)
     }
   }
   </script>
@@ -28,6 +93,9 @@
   }
   a {
     color: #42b983;
+  }
+  h1 {
+    padding-bottom: 20px;
   }
   </style>
   
